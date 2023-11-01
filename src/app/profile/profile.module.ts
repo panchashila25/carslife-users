cf728b968e6329fe7c68ea7e58cf1cd7f5fad0e5
Darkunde mahesh <mahesh.darkunde@tectiq.in>
2023-11-01 15:48:28 +0530

    m

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfileComponent],
   imports: [
     CommonModule,
+    IonicModule.forRoot(),
     ProfileRoutingModule
   ]
 })
