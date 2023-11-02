9f5061653d8942606792582408393f8e2109cc80
shubham-zambre <shubhamzambre11@gmail.com>
2023-11-02 16:02:38 +0530

    booking

--- src/app/bookings/bookings.module.ts
+++ src/app/bookings/bookings.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular';
 import { BookingsRoutingModule } from './bookings-routing.module';
+import { BookingsComponent } from './bookings.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookingsComponent
+  ],
   imports: [
     CommonModule,
-    BookingsRoutingModule
+    BookingsRoutingModule,
+    CommonModule,
+    IonicModule.forRoot()
+
   ]
 })
 export class BookingsModule { }
