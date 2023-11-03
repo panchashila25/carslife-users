7e292fce90957aa76b13760edd629baf7815972c
mahesh42646 <maheshdarkunde2@gmail.com>
2023-11-03 16:13:43 +0530

    ad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TabsComponent } from './tabs/tabs.component';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+// import { Geolocation } from '@ionic-native/geolocation/ngx';
+// import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, TabsComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [
-    Geolocation ,
-    NativeGeocoder,
-
+  
 
     { provide: RouteReuseStrategy , useClass: IonicRouteStrategy,  }
   ],
